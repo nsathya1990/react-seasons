436e55db3b559f5826a72e862764287f44a61125
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-05-01 16:00:51 +0530

    udpating state properties

--- src/index.js
+++ src/index.js
@@ -15,17 +15,28 @@ class App extends React.Component {
         super(props);
 
         this.state = {
-            lat: null
+            lat: null,
+            errorMessage: ''
         };
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
     }
 
     /** React says we have to define render!! */
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+        return (
+            <div>
+                Latitude: {this.state.lat}
+                <br />
+                Error: {this.state.errorMessage}
+            </div>
         );
-        return <div>Latitude:</div>;
     }
 }
 
